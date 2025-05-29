fb4234fdb4e379c68810a59f10f2de30fa8d7688
s4174203 <s4174203@student.rmit.edu.au>
2025-05-29 16:44:39 +1000

    Update Map.java

--- code/data/Map.java
+++ code/data/Map.java
@@ -4,31 +4,60 @@ import utils.*;
 
 public class Map {
 
-    private String title;
-    private String script;
+
     public static int X;
     public static int Y;
-    
-    public Map(String title, String script){
+    public static String names[][];
+    public static String descs[][];
+    public static Boolean requiresBoat[][];
+    public static Boolean requiresSpyGlass[][];
 
-        this.title = title;
-        this.script = script;
-        this.X = X;
-        this.Y = Y;
+    public Map(int x, int y) {
 
+        String names[][] = null;
+        String descs[][] = null;
+        Boolean requiresBoat[][] = null;
+        Boolean requiresSpyGlass[][] = null;
+
+        names[0][0] = "TITLE";
+        descs[0][0] = "DESCRIPTION";
+        requiresBoat[0][0] = false;
+        requiresSpyGlass[0][0] = false;
+
+        names[0][0] = "TITLE";
+        descs[0][0] = "DESCRIPTION";
+        requiresBoat[0][0] = false;
+        requiresSpyGlass[0][0] = false;
+
+        names[0][0] = "TITLE";
+        descs[0][0] = "DESCRIPTION";
+        requiresBoat[0][0] = false;
+        requiresSpyGlass[0][0] = false;
+
+        names[0][0] = "TITLE";
+        descs[0][0] = "DESCRIPTION";
+        requiresBoat[0][0] = false;
+        requiresSpyGlass[0][0] = false;
+
+        names[0][0] = "TITLE";
+        descs[0][0] = "DESCRIPTION";
+        requiresBoat[0][0] = false;
+        requiresSpyGlass[0][0] = false;
     }
 
-    public void Cove(){
-        title = "Cove";
-        script = "Help me";
-        X = 0;
-        Y = 0;
+    public static String getName(int x, int y) {
+        return names[x][y];
     }
 
-    public static boolean needsBoat(int newX, int newY) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'needsBoat'");
+    public static String getDescs(int x, int y) {
+        return descs[x][y];
     }
-    
 
+    public static boolean needsBoat(int x, int y) {
+        return requiresBoat[x][y];
+    }
+
+    public static boolean needsSpyGlass(int x, int y) {
+        return requiresSpyGlass[x][y];
+    }
 }
